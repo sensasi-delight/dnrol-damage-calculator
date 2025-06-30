ef9c070f4c49ff3bb6b138a75fd1cea00352f406
Zain🍕 <zainadam.id@gmail.com>
2025-06-30 23:17:19 +0800

    Add `<PetSelect />` input

---
+++ src/lib/components/damage-calculator/pet-select.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	// vendors
+	import { titleCase } from 'title-case';
+	// uis
+	import { Select, SelectContent, SelectItem, SelectTrigger } from '$lib/components/ui/select';
+	import { Label } from '$lib/components/ui/label';
+	// data
+	import { petNames } from '$lib/dataset/pets';
+
+	let { value: selectedPet } = $props();
+</script>
+
+<Label for="pet">Choose Your Pet</Label>
+<Select type="single" bind:value={selectedPet}>
+	<SelectTrigger class="w-full">{selectedPet ? titleCase(selectedPet) : 'Select pet'}</SelectTrigger
+	>
+	<SelectContent>
+		{#each petNames as petName}
+			<SelectItem value={petName}>{titleCase(petName)}</SelectItem>
+		{/each}
+	</SelectContent>
+</Select>
