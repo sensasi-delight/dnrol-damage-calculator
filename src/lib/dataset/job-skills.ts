8df7ebc0b5d3215e690508dd4a5edb9440d3509e
Zain🍕 <zainadam.id@gmail.com>
2025-07-01 03:01:45 +0800

    Add skills dataset

--- src/lib/dataset/job-skills.ts
+++ src/lib/dataset/job-skills.ts
@@ -1,16 +1,24 @@
 import type { Job } from './jobs';
-import priestSkills from './skills/priest';
 import type Skill from './skills/types/skill';
+// dataset
+import acrobatSkills from './skills/acrobat.csv';
+import bowMasterSkills from './skills/bow master.csv';
+import elementalLordSkills from './skills/elemental lord.csv';
+import forceUserSkills from './skills/force user.csv';
+import mercenarySkills from './skills/mercenary.csv';
+import paladinSkills from './skills/paladin.csv';
+import priestSkills from './skills/priest.csv';
+import swordsmanSkills from './skills/swordsman.csv';
 
 const jobSkills: { [key in Job]: Skill[] } = {
+	acrobat: acrobatSkills,
+	'bow master': bowMasterSkills,
+	'elemental lord': elementalLordSkills,
+	'force user': forceUserSkills,
+	mercenary: mercenarySkills,
 	priest: priestSkills,
-	paladin: priestSkills,
-	acrobat: priestSkills,
-	'bow master': priestSkills,
-	swordsman: priestSkills,
-	mercenary: priestSkills,
-	'force user': priestSkills,
-	'elemental lord': priestSkills
+	paladin: paladinSkills,
+	swordsman: swordsmanSkills
 };
 
 export default jobSkills;
